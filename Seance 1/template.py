c5cf9173b135ac5f475f39b345033613fcdcd212
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-11-22 21:03:20 +0100

    fixed same bug in template file

--- Seance 1/template.py
+++ Seance 1/template.py
@@ -1,7 +1,7 @@
 #L'objectif est de trouver la liste des pays contenants un string (stringToFind)
 def main(stringToFind):
     ### Ne pas toucher ###
-    with open("Seance 1/country_list.txt", "r", encoding="utf-8") as file:
+    with open("country_list.txt", "r", encoding="utf-8") as file:
         allCountries = file.read()
     allCountries = allCountries.splitlines()
     allCountries = [country[:-5] for country in allCountries]
